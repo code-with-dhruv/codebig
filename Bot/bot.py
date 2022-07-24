54eed4995f30d51b46bf9bc8e0655bcd9c75a371
dhruv <87694090+code-with-dhruv@users.noreply.github.com>
2022-07-24 23:21:17 +0530

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -196,8 +196,6 @@ def solve(update,context):
 }
                 response = requests.post('http://13.234.234.30:5000/validateSubmission', headers=headers, json=json_data, verify=False)
                 q=response.json()
-                text=str(q)
-                SendMess(players,text)
                 text=("<b>Your Score</b> - {}".format(q['score']))
                 Sendmessage(chat_id,text)
                 w=q['result']
